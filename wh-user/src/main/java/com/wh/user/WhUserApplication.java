0e2281a8c5669a37de6439e53c2293309f896cb8
保安 <981585755@qq.com>
2018-04-20 17:17:42 +0800

    新增配置服务器

--- wh-user/src/main/java/com/wh/user/WhUserApplication.java
+++ wh-user/src/main/java/com/wh/user/WhUserApplication.java
@@ -2,11 +2,13 @@ package com.wh.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class WhUserApplication {
 
     public static void main(String[] args) {
