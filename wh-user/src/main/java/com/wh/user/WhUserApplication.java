0ebe9ff0e01d58b9bf9a23b2ead7c741b8e7079b
保安 <981585755@qq.com>
2018-05-08 15:52:32 +0800

    完成对配置中心的配置

--- wh-user/src/main/java/com/wh/user/WhUserApplication.java
+++ wh-user/src/main/java/com/wh/user/WhUserApplication.java
@@ -3,7 +3,6 @@ package com.wh.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
--- wh-user/src/main/java/com/wh/user/config/DruidSource.java
+++ wh-user/src/main/java/com/wh/user/config/DruidSource.java
@@ -6,6 +6,7 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
